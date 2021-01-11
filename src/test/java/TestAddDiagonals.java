bb435244975e263d600718cf71012365590b55f9
Rachel Trana <raxytrana@gmail.com>
2021-01-10 23:05:35 -0600

    Modifying README and adding tests

---
+++ src/test/java/TestAddDiagonals.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TestAddDiagonals {
+    @Test
+    void shouldFindSumForOddMatrix() {
+        int[][] mat = { {1, 0, 2}, {9, 1, 4}, {-8, 3, 10 } };
+//        assertEquals(6, Problem1.addDiagonals(mat));
+    }
+
+    @Test
+    void shouldFindSumForEvenMatrix() {
+        int[][] mat = { {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1}, {1, 1, 1, 1} };
+//        assertEquals(8, Problem1.addDiagonals(mat));
+    }
+
+    @Test
+    void shouldFindSumForSmallMatrix() {
+        int[][] mat = {{5}};
+//        assertEquals(5, Problem1.addDiagonals(mat));
+    }
+}
